236e9c2c3e383a584cbb2f9e5357c7fe2192c182
Porytska <94567121+Porytska@users.noreply.github.com>
2021-11-17 22:13:01 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Людмилко on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        print("\(digit) was touched")
+        }
+    }
+
+    
+
